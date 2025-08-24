220c8c445aba36bfdb522d0e2b0a3ac9bbf2c94e
KARTIK5799 <thekartiksapkal@gmail.com>
2025-08-24 12:19:49 +0530

    Theme removed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from './context/ThemeContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
@@ -8,9 +7,9 @@ import App from './App.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
+
         <App />
-      </ThemeProvider>
+
 
     </BrowserRouter>
   </StrictMode>,
